ffa3c72a0bb528e95106ce74fa9699c38353e14d
Brian Quach <13rianquach@gmail.com>
2015-09-24 00:05:54 -0700

    refactor: tournament tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -12,26 +12,19 @@ CREATE TABLE player (
     name varchar(1000) NOT NULL,
     tournamentId integer REFERENCES tournament(id)
 );
-CREATE TABLE outcome (
-    id serial PRIMARY KEY,
-    winnerId integer REFERENCES player(id),
-    loserId integer REFERENCES player(id),
-    isTie boolean NULL
-);
 CREATE TABLE match (
     id serial PRIMARY KEY,
-    roundNumber integer NOT NULL,
-    playerOneId integer REFERENCES player(id), 
-    playerTwoId integer REFERENCES player(id),
-    outcomeId integer REFERENCES outcome(id)
+    tournamentId integer REFERENCES tournament(id),
+    winnerId integer REFERENCES player(id), 
+    loserId integer REFERENCES player(id)
 );
 
 -- View definitions
--- CREATE VIEW playerStanding AS (
---     SELECT p.id, COUNT(o.winnder) as wins, as losses, as ties, p.tournamentId
---     FROM player p, match m, outcome o
---     WHERE (p.id = m.playerOneId OR p.id = m.playerTwoId) 
---         AND (p.tournamentId = m.tournamentId)
---         AND (m.outcomeId = o.id)
---     GROUP BY p.tournamentId, p.id
--- );
+CREATE VIEW playerStanding AS (
+    SELECT p.id, p.name, COUNT(m.winnerId) AS wins, COUNT(m.winnerId) + COUNT(m2.loserID) AS matches
+    FROM player p
+        LEFT JOIN match m ON p.id = m.winnerId
+        LEFT JOIN match m2 ON p.id = m2.loserId
+    GROUP BY p.id, m.winnerId, m2.loserId
+    ORDER BY wins DESC
+);
