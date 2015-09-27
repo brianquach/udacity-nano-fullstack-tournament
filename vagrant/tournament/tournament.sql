d304ac1c19631751a970ae18f9edb2bfa17f5223
Brian Quach <13rianquach@gmail.com>
2015-09-26 18:55:08 -0700

    Feat: change sql for 'match tie'
    Add view playerStandingWithTie to keep backwards compatibility with original
    udacity test case using playerStanding. Change sql scheme to account for match
    ties.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -12,19 +12,35 @@ CREATE TABLE player (
     name varchar(1000) NOT NULL,
     tournamentId integer REFERENCES tournament(id)
 );
+CREATE TABLE match_tie (
+    id serial PRIMARY KEY,
+    playerOneId integer REFERENCES player(id),
+    playerTwoId integer REFERENCES player(id)
+);
 CREATE TABLE match (
     id serial PRIMARY KEY,
     tournamentId integer REFERENCES tournament(id),
-    winnerId integer REFERENCES player(id), 
-    loserId integer REFERENCES player(id)
+    winnerId integer REFERENCES player(id) NULL, 
+    loserId integer REFERENCES player(id) NULL,
+    tieId integer REFERENCES match_tie(id) NULL
 );
 
 -- View definitions
 CREATE VIEW playerStanding AS (
-    SELECT p.id, p.name, COUNT(m.winnerId) AS wins, COUNT(m.winnerId) + COUNT(m2.loserID) AS matches
+    SELECT p.id, p.name, COUNT(m.winnerId) AS wins, COUNT(m.winnerId) + COUNT(m2.loserID) + COUNT(mt.id) AS matches
     FROM player p
         LEFT JOIN match m ON p.id = m.winnerId
         LEFT JOIN match m2 ON p.id = m2.loserId
-    GROUP BY p.id, m.winnerId, m2.loserId
+        LEFT JOIN match_tie mt ON p.id = mt.playerOneId OR p.id = mt.playerTwoId
+    GROUP BY p.id, m.winnerId, m2.loserId, mt.id
+    ORDER BY wins DESC
+);
+CREATE VIEW playerStandingWithTies AS (
+    SELECT p.id, p.name, COUNT(m.winnerId) AS wins, COUNT(m.winnerId) + COUNT(m2.loserID) + COUNT(mt.id) AS matches, COUNT(mt.id) AS ties
+    FROM player p
+        LEFT JOIN match m ON p.id = m.winnerId
+        LEFT JOIN match m2 ON p.id = m2.loserId
+        LEFT JOIN match_tie mt ON p.id = mt.playerOneId OR p.id = mt.playerTwoId
+    GROUP BY p.id, m.winnerId, m2.loserId, mt.id
     ORDER BY wins DESC
 );
